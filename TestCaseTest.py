1775c59b67f8bea7f410160fcff12eaaffaecd3f
Yoon-Hong-Chan <ychqwertyuio@gmail.com>
2022-09-29 00:52:05 +0900

    feat : 20장 뒷정리하기 (teardown 테스트)

--- TestCaseTest.py
+++ TestCaseTest.py
@@ -2,16 +2,10 @@ from WasRun import *
 
 
 class TestCaseTest(TestCase):
-    def setUp(self):
-        self.test = WasRun("testMethod")
-
-    def testRunning(self):
-        self.test.run()
-        assert (self.test.wasRun)
-
-    def testSetUp(self):
-        self.test.run()
-        assert (self.test.wasSetUp)
+    def testTemplateMethod(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert("setUp testMethod tearDown " == test.log)
 
 
-TestCaseTest("testSetUp").run()
+TestCaseTest("testTemplateMethod").run()
